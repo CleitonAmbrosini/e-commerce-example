fc11f1a77a0a97889c6618481607eb80d371a8da
Cleiton Ambrosini <cleiton.ambrosini@softplan.com.br>
2022-10-22 19:56:35 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      // required due to custom location of tsconfig.json configuration file
+      // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+      { tsconfig: './config/tsconfig.json' },
+    ],
+  },
+};
